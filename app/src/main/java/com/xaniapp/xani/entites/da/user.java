097fcb7784098cc147b0ba841b4a091f9856dc32
Jonathan Campbell Jones <jonathan.cj@ntlworld.com>
2024-09-05 13:22:01 +0100

    Added a joined date to the user, and the post field names are now consistent. Createda database business class to hold the database functioanaity, the feed is now merged into the database. User didn't commit. don't know why.

--- app/src/main/java/com/xaniapp/xani/entites/da/user.java
+++ app/src/main/java/com/xaniapp/xani/entites/da/user.java
@@ -1,19 +1,35 @@
 package com.xaniapp.xani.entites.da;
 
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "user")
-    public class User {
+import com.xaniapp.xani.entites.api.FeedResponse;
 
-        @PrimaryKey(autoGenerate = false)
-        public long u_id;
-        @ColumnInfo(name = "u_username")
-        public String u_username;
-        @ColumnInfo(name = "u_email_address")
-        public String u_email_address;
-        @ColumnInfo(name = "u_password_hash")
-        public String u_password_hash;
+import java.util.Date;
+
+@Entity(tableName = "user")
+public class User {
+
+    @PrimaryKey(autoGenerate = false)
+    public long u_id;
+    @ColumnInfo(name = "u_username")
+    public String u_username;
+    @ColumnInfo(name = "u_avitar")
+    public String u_avitar;
+    @ColumnInfo(name = "u_description")
+    public String u_description;
+    public @ColumnInfo(name = "u_joined_date")
+    @Nullable Date u_joined_date;
+
+    public User() {}
+    public User(FeedResponse.UserItem apiUser) {
+        this.u_id = apiUser.u_id;
+        this.u_username = apiUser.u_username;
+        this.u_avitar = apiUser.u_avitar;
+        this.u_description = apiUser.u_description;
+        this.u_joined_date = apiUser.u_joined_date;
+    }
 }
 
